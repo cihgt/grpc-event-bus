93c3e1b620e7bf5de6c7715be6fa6a692e48c255
cihgt <dmitriyyych@gmail.com>
2025-05-10 14:58:34 +0300

    proper logging added

--- internal/subscription-service/subscription-service.go
+++ internal/subscription-service/subscription-service.go
@@ -3,7 +3,6 @@ package subscription_service
 import (
 	"context"
 	"errors"
-	"fmt"
 	_ "google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -25,20 +24,26 @@ func NewServer(b *subpub.Bus, l *slog.Logger) server {
 
 // Subscribe подписывает на событие
 func (s server) Subscribe(req *proto.SubscribeRequest, stream proto.PubSub_SubscribeServer) error {
-	s.logger.Info("Новая подписка: " + req.Key)
+	s.logger.Info("Получена новая подписка", slog.String("key", req.Key))
 
 	// канал пришедших событий
 	events := make(chan string, 24)
 	sub, err := s.bus.Subscribe(req.Key, func(msg interface{}) {
 		strMsg, ok := msg.(string)
 		if !ok {
-			s.logger.Warn("получено не строковое сообщение")
+			s.logger.Warn("Получено не строковое сообщение",
+				slog.String("key", req.Key),
+				slog.Any("message", msg),
+			)
 		} else {
 			events <- strMsg
 		}
 	})
 	if err != nil {
-		s.logger.Warn(fmt.Sprintf("ошибка подписки: %v", err))
+		s.logger.Error("Ошибка при подписке",
+			slog.String("key", req.Key),
+			slog.Any("error", err),
+		)
 		return status.Errorf(codes.Internal, "ошибка подписки: %v", err)
 	}
 	defer sub.Unsubscribe()
@@ -46,11 +51,14 @@ func (s server) Subscribe(req *proto.SubscribeRequest, stream proto.PubSub_Subsc
 		select {
 		case data := <-events:
 			if err := stream.Send(&proto.Event{Data: data}); err != nil {
-				s.logger.Warn(fmt.Sprintf("ошибка отправки события: %v", err))
+				s.logger.Error("Ошибка отправки события",
+					slog.String("key", req.Key),
+					slog.Any("error", err),
+				)
 				return status.Errorf(codes.Unavailable, "не удалось отправить событие: %v", err)
 			}
 		case <-stream.Context().Done():
-			s.logger.Warn(fmt.Sprintf("подписка отменена клиентом: %v", err))
+			s.logger.Debug("Подписка отменена клиентом", slog.String("key", req.Key))
 			return status.Error(codes.Canceled, "подписка отменена клиентом")
 		}
 	}
@@ -60,11 +68,22 @@ func (s server) Subscribe(req *proto.SubscribeRequest, stream proto.PubSub_Subsc
 func (s server) Publish(ctx context.Context, req *proto.PublishRequest) (*emptypb.Empty, error) {
 	if err := s.bus.Publish(req.Key, req.Data); err != nil {
 		if errors.Is(err, subpub.ErrBusClosed) {
-			s.logger.Warn(fmt.Sprintf("запись в закрытую шину: %v", err))
+			s.logger.Error("Попытка записи в закрытую шину",
+				slog.String("key", req.Key),
+				slog.Any("error", err),
+			)
 			return nil, status.Errorf(codes.Unavailable, "шина закрыта")
 		}
-		s.logger.Warn(fmt.Sprintf("ошибка публикации: %v", err))
+		s.logger.Error("Ошибка публикации сообщения",
+			slog.String("key", req.Key),
+			slog.String("data", req.Data),
+			slog.Any("error", err),
+		)
 		return nil, status.Errorf(codes.Internal, "ошибка публикации: %v", err)
 	}
+	s.logger.Info("Сообщение успешно опубликовано",
+		slog.String("key", req.Key),
+		slog.String("data", req.Data),
+	)
 	return &emptypb.Empty{}, nil
 }
